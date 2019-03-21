a3c9a5f5778ff39565f010a204924bd85c2d6d7d
ispicer <iospicer@gmail.com>
2019-03-21 17:34:49 -0400

    Temp

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://pi-lit-db-user:EBQ0fF6WUD9TLQjM@cis-db-fxsbk.mongodb.net/test?retryWrites=true"
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+	client.connect(err => {
+ 		const collection = client.db("Pi-Lit").collection("users");
+  		collection.find({}).toArray(function(err, result) {
+  			if (err) throw err;
+  			console.log(result);
+  			client.close();
+  		})
+	});
 });
+
+client.connect(err => {
+	const collection = client.db("Pi-Lit").collection("users");
+	collection.find({}).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		client.close();
+	})
+});
+
+
 
 module.exports = router;
